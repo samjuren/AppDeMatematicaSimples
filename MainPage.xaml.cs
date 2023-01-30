a65c75fb3324fa25e077b38809e8807d44350f71
sammj <sammj@SAMUEL>
2023-01-30 18:09:43 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace AppDeMatematicaSimples;
+
+public partial class MainPage : ContentPage
+{
+    int Num1, Num2, result;
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+    public void Btn_somar_Clicked(object sender, EventArgs e)
+    {   
+        if (Rd_Somar.IsChecked)
+        {
+            Num1 = int.Parse(Number1.Text);
+            Num2 = int.Parse(Number2.Text);
+            result = Num1 + Num2;
+
+            DisplayAlert("SOMA", result.ToString(), "OK");
+        }
+        else if (Rd_Mult.IsChecked)
+        {
+            Num1 = int.Parse(Number1.Text);
+            Num2 = int.Parse(Number2.Text);
+            result = Num1 / Num2;
+
+            DisplayAlert("Divisão", result.ToString(), "OK");
+        }
+        else if (Rd_Sub.IsChecked)
+        {
+            Num1 = int.Parse(Number1.Text);
+            Num2 = int.Parse(Number2.Text);
+            result = Num1 - Num2;
+
+            DisplayAlert("Subtração", result.ToString(), "OK");
+        }
+        else
+        {
+            Num1 = int.Parse(Number1.Text);
+            Num2 = int.Parse(Number2.Text);
+            result = Num1 * Num2;
+
+            DisplayAlert("Multipliacação", result.ToString(), "OK");
+        }
+
+    }
+}
+
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppDeMatematicaSimples;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
